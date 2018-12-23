5e4c53e48adcb82c47c7c28356ea12960c3966f5
ss100yan <32798618+ss100yan@users.noreply.github.com>
2018-12-23 14:15:17 -0500

    new

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,11 @@
 
 
-var number = 0;
+var number = Math.floor(Math.random() * 120 ) ;
 var wins = 0;
 var losses = 0;
+var number2 = 0;
+var score = 0;
+
 
 // var Text = document.getElementById("number")
 // Text.innerHTML("number");
@@ -11,5 +14,6 @@ $(document).ready(function(){
 $("#number").html(number);
 $("#wins").html(wins);
 $("#losses").html(losses);
-
+$("#number2").html(number2);
+$("#score").html(score);
 });
