e16c3410b357de456196610e87794dcbd117c8d8
ss100yan <32798618+ss100yan@users.noreply.github.com>
2018-12-23 13:41:26 -0500

    new

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+
+
+var number = 0;
+var wins = 0;
+var losses = 0;
+
+// var Text = document.getElementById("number")
+// Text.innerHTML("number");
+
+$(document).ready(function(){
+$("#number").html(number);
+$("#wins").html(wins);
+$("#losses").html(losses);
+
+});
