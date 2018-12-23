5ba756ac02ee307f5e51d3bd21ee8222f4b8b8ae
ss100yan <32798618+ss100yan@users.noreply.github.com>
2018-12-23 14:29:29 -0500

    new

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,19 @@
 
 
-var number = Math.floor(Math.random() * 120 ) ;
+var number = Math.floor(Math.random() * (120 - 19 + 1) ) + 19;
 var wins = 0;
 var losses = 0;
 var number2 = 0;
 var score = 0;
 
-
-// var Text = document.getElementById("number")
-// Text.innerHTML("number");
+var cristal1=Math.floor(Math.random() * 12 );
+var cristal2=Math.floor(Math.random() * 12 );
+var cristal3=Math.floor(Math.random() * 12 );
+var cristal4=Math.floor(Math.random() * 12 );
 
 $(document).ready(function(){
 $("#number").html(number);
 $("#wins").html(wins);
 $("#losses").html(losses);
-$("#number2").html(number2);
 $("#score").html(score);
 });
