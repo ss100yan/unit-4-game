da7cea0a43298feddeb56af022e0f6e1d436daaf
ss100yan <32798618+ss100yan@users.noreply.github.com>
2018-12-24 00:28:16 -0500

    new

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,25 +12,25 @@ var cristal3=Math.floor(Math.random() * 12 );
 var cristal4=Math.floor(Math.random() * 12 );
 
 $(document).ready(function(){
-$("#number").html(number);
-$("#wins").html(wins);
-$("#losses").html(losses);
-$("#score").html(score);
-$("#img1").css( 'cursor', 'pointer' ).click( function(){score=score+cristal1; play();
-   
-    } );
-$("#img2").css( 'cursor', 'pointer' ).click(function(){score=score+cristal2; play();
-   
-      } );
-$("#img3").css( 'cursor', 'pointer' ).click(function(){score=score+cristal3; play(); 
-    
-     });
-$("#img4").css( 'cursor', 'pointer' ).click(function(){score=score+cristal4; play();
-    
-     });
-    var play= function(){
-        if( score == number ){wins++; score=0;}else if(score > number) {losses++; score=0;};
-    $("#wins").html(wins);$("#losses").html(losses);$("#score").html(score);
-    };
+ $("#number").html(number);
+   $("#wins").html(wins);
+    $("#losses").html(losses);
+     $("#score").html(score);
 
+$("#img1").css( 'cursor', 'pointer' ).click(function(){score=score+cristal1; play();});
+   $("#img2").css( 'cursor', 'pointer' ).click(function(){score=score+cristal2; play();});
+     $("#img3").css( 'cursor', 'pointer' ).click(function(){score=score+cristal3; play();});
+       $("#img4").css( 'cursor', 'pointer' ).click(function(){score=score+cristal4; play();});
+        var play= function(){
+         if( score == number ){wins++; score=0; reset();}else if(score > number) {losses++; score=0; reset();};
+          $("#wins").html(wins);$("#losses").html(losses);$("#score").html(score);
+             };
+              var reset = function(){
+                number = Math.floor(Math.random() * (120 - 19 + 1) ) + 19;
+                 cristal1=Math.floor(Math.random() * 12 );
+                   cristal2=Math.floor(Math.random() * 12 );
+                    cristal3=Math.floor(Math.random() * 12 );
+                      cristal4=Math.floor(Math.random() * 12 );
+                        $("#number").html(number);
+             };
 });
