ef97a6ac6491111b4d53a9563cf52caf8dd64a3a
ss100yan <32798618+ss100yan@users.noreply.github.com>
2018-12-23 22:43:54 -0500

    new

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,11 @@
 
 
-var number = Math.floor(Math.random() * (120 - 19 + 1) ) + 19;
+
 var wins = 0;
 var losses = 0;
 var number2 = 0;
 var score = 0;
-
+var number = Math.floor(Math.random() * (120 - 19 + 1) ) + 19;
 var cristal1=Math.floor(Math.random() * 12 );
 var cristal2=Math.floor(Math.random() * 12 );
 var cristal3=Math.floor(Math.random() * 12 );
@@ -16,20 +16,21 @@ $("#number").html(number);
 $("#wins").html(wins);
 $("#losses").html(losses);
 $("#score").html(score);
-$("#img1").css( 'cursor', 'pointer' ).click( function(){score=score+cristal1; 
-    if( score == number ){wins++}else if(score > number) {losses++};
-    $("#wins").html(wins);$("#losses").html(losses);$("#score").html(score);
+$("#img1").css( 'cursor', 'pointer' ).click( function(){score=score+cristal1; play();
+   
     } );
-$("#img2").css( 'cursor', 'pointer' ).click(function(){score=score+cristal2; 
-     if( score == number ){wins++}else if(score > number) {losses++};
-     $("#wins").html(wins);$("#losses").html(losses);$("#score").html(score);
+$("#img2").css( 'cursor', 'pointer' ).click(function(){score=score+cristal2; play();
+   
       } );
-$("#img3").css( 'cursor', 'pointer' ).click(function(){score=score+cristal3; 
-    if( score == number ){wins++}else if(score > number) {losses++};
-    $("#wins").html(wins);$("#losses").html(losses);$("#score").html(score);
+$("#img3").css( 'cursor', 'pointer' ).click(function(){score=score+cristal3; play(); 
+    
      });
-$("#img4").css( 'cursor', 'pointer' ).click(function(){score=score+cristal4;
-    if( score == number ){wins++}else if(score > number) {losses++};
-    $("#wins").html(wins);$("#losses").html(losses);$("#score").html(score);
+$("#img4").css( 'cursor', 'pointer' ).click(function(){score=score+cristal4; play();
+    
      });
+    var play= function(){
+        if( score == number ){wins++; score=0;}else if(score > number) {losses++; score=0;};
+    $("#wins").html(wins);$("#losses").html(losses);$("#score").html(score);
+    };
+
 });
